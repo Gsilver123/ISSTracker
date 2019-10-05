9a1f3c7960066ed46822e8ea320ada6f993f487e
Garrett Silver <Gsilver@buffalo.edu>
2019-10-05 18:06:35 -0400

    HomePage SetUp (#1)
    Created the application colors, as well as created strings for the homepage. Initialized the view variables in the MainActivity.java. Placed mock lat and long in the correct places.

--- app/src/main/java/com/example/isstracker/MainActivity.java
+++ app/src/main/java/com/example/isstracker/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.isstracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mLatitudeTextView;
+    private TextView mLongitudeTextView;
+    private Button mViewOnMapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLatitudeTextView = findViewById(R.id.latitude_position_text_view);
+        mLongitudeTextView = findViewById(R.id.longitude_position_text_view);
+        mViewOnMapButton = findViewById(R.id.view_on_map_btn);
     }
 }
